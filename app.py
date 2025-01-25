9bf2a285e7841e3dc9c6c009777e19ede8f8b93f
djabjo <vovakotov971@gmail.com>
2025-01-25 23:42:15 +0700

    наброска сайта

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+db =  SQLAlchemy(app)
+
 
 @app.route('/index')
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/posts")
+def posts():
+    posts = Post.query.all()
+    return render_template("posts.html", posts=posts)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+@app.route('/create', methods=["POST", "GET"])
+def create():
+    if request.method == "POST":
+        title = (request.form['title'])
+        text = (request.form.getlist('text')[0])
+
+        post = Post(title=title, text=text)
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/index')
+        except:
+            return "При добавлении статьи произошла ошибка!"
+    else:
+        return render_template("create.html")
+
+
 
 @app.route("/about_project")
 def about_project():
